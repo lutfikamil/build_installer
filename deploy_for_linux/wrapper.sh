740b35889f98ce3db8924018a8f7bd53e46add77
lutfikamil <61917514+lutfikamil@users.noreply.github.com>
2025-02-25 23:59:37 +0700

    Update wrapper.sh
    update 2

--- deploy_for_linux/wrapper.sh
+++ deploy_for_linux/wrapper.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 set -e
 
-# Cari executable di dalam bin/
-EXECUTABLE=$(find bin/ -maxdepth 1 -type f -executable ! -name "*.sh" | head -n 1)
+EXECUTABLE="Sultan"
 
 if [ -z "$EXECUTABLE" ]; then
-    echo "❌ Tidak ada file eksekusi yang ditemukan dalam bin/"
+    echo "❌ wrapper.sh Tidak ada file eksekusi yang ditemukan dalam $(pwd)"
     exit 1
 fi
 
 EXECUTABLE_NAME=$(basename "$EXECUTABLE")
-INSTALL_FOLDER_NAME=$(echo "$EXECUTABLE_NAME" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')"-v1.0"
-
-TMP_DIR="/tmp/${INSTALL_FOLDER_NAME}-installer"
-
+INSTALL_FOLDER_NAME=$(echo "$EXECUTABLE_NAME" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')"-lm"
+TMP_DIR="/tmp/${INSTALL_FOLDER_NAME}-v1.0-installer"
+echo "$INSTALL_FOLDER_NAME"
+echo "$EXECUTABLE_NAME"
+echo "$TMP_DIR"
+echo "🗑 Membersihkan instalasi sebelumnya..."
 if [ -d "$TMP_DIR" ]; then
-    echo "🗑 Membersihkan instalasi sebelumnya..."
     rm -rf "$TMP_DIR"
 fi
 
 mkdir -p "$TMP_DIR"
-DATA_ARCHIVE="$(dirname "$0")/data.tar.gz"
+BIN_ARCHIVE="$(dirname "$0")/bin.tar.gz"
 
-if [ ! -f "$DATA_ARCHIVE" ]; then
-    echo "❌ File data.tar.gz tidak ditemukan!"
+if [ ! -f "$BIN_ARCHIVE" ]; then
+    echo "❌ File bin.tar.gz tidak ditemukan!"
     exit 1
 fi
 
-tar xf "$DATA_ARCHIVE" -C "$TMP_DIR"
+tar xf "$BIN_ARCHIVE" -C "$TMP_DIR"
 
-cd "$TMP_DIR" || exit 1
+cd "$TMP_DIR/bin" || exit 1
 echo "📂 Isi folder sementara:"
 ls -l "$TMP_DIR"
 
-# Pastikan install.sh bisa dieksekusi
-chmod +x "$TMP_DIR/install.sh"
-
-# Jalankan instalasi
-exec "$TMP_DIR/install.sh"
+chmod +x "$TMP_DIR/bin/install.sh"
+exec "$TMP_DIR/bin/install.sh"
